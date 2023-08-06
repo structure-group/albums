52d902f365b95cd0a9e618c647c633560376eae1
Matilde Park <matilde@park.net>
2023-08-05 22:58:59 -0700

    app: port silo's s3 init

--- ui/src/app.jsx
+++ ui/src/app.jsx
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from "react";
-import Urbit from "@urbit/http-api";
-
-const api = new Urbit("", "", window.desk);
-api.ship = window.ship;
+import { api } from "./state/api";
+import useStorageState from "./state/storage";
 
 export function App() {
+  const { s3 } = useStorageState();
+  const credentials = s3.credentials;
+  const configuration = s3.configuration;
   const [albums, setAlbums] = useState();
+
 
   useEffect(() => {
     async function init() {
+      useStorageState.getState().initialize(api);
       // albums are an array of arrays, each sub-array is a tuple of album id and owner
       // eg. [['beach', '~zod'], ...]
       const albums = (
@@ -23,6 +26,19 @@ export function App() {
 
     init();
   }, []);
+
+  useEffect(() => {
+    const hasCredentials =
+      credentials?.accessKeyId &&
+      credentials?.endpoint &&
+      credentials?.secretAccessKey && configuration;
+    if (hasCredentials) {
+      // createClient(credentials, configuration.region);
+
+      useStorageState.setState({ hasCredentials: true });
+      console.log("client initialized");
+    }
+  }, [credentials, configuration]);
 
   return (
     <main className="flex items-center justify-center min-h-screen"></main>
