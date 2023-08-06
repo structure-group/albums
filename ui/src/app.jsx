f1fc5bf1e3373bd17212697e208b5b2f6388daf5
Matilde Park <matilde@park.net>
2023-08-05 23:30:14 -0700

    state, lib: port useFileStore from silo
    I had to remove some logic so the endpoint protocol is rendered, eg. "https:", not "https",
    or else S3Client would connect to "https//s3..." and crash.
    Silo definitely works, so why do we need to change that here?

--- ui/src/app.jsx
+++ ui/src/app.jsx
@@ -1,11 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import { api } from "./state/api";
 import useStorageState from "./state/storage";
+import { useFileStore } from "./state/useFileStore";
+import { useAsyncCall } from "./lib/useAsyncCall";
 
 export function App() {
   const { s3 } = useStorageState();
   const credentials = s3.credentials;
   const configuration = s3.configuration;
+  const { client, createClient, getFiles } = useFileStore();
+  const { call: loadFiles } = useAsyncCall(
+    useCallback(async (s3) => {
+      return getFiles(s3);
+    }, []),
+    useFileStore
+  );
   const [albums, setAlbums] = useState();
 
 
@@ -33,12 +42,16 @@ export function App() {
       credentials?.endpoint &&
       credentials?.secretAccessKey && configuration;
     if (hasCredentials) {
-      // createClient(credentials, configuration.region);
+      createClient(credentials, configuration.region);
 
       useStorageState.setState({ hasCredentials: true });
       console.log("client initialized");
     }
   }, [credentials, configuration]);
+
+  useEffect(() => {
+    loadFiles(s3);
+  }, [loadFiles, s3, client]);
 
   return (
     <main className="flex items-center justify-center min-h-screen"></main>
