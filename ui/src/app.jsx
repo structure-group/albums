4ff718957818bde2ab39709da311e1a2c1b7c3ff
Matilde Park <matilde@park.net>
2023-09-12 12:47:34 -0400

    app: example actions and example scry

--- ui/src/app.jsx
+++ ui/src/app.jsx
@@ -31,6 +31,13 @@ export function App() {
       )?.["album-ids"];
       setAlbums(albums);
       console.log(albums);
+      const photos = (
+        await api.scry({
+          app: "albums",
+          path: "/album/string/~zod",
+        })
+      );
+      console.log(photos);
     }
 
     init();
@@ -54,6 +61,53 @@ export function App() {
   }, [loadFiles, s3, client]);
 
   return (
-    <main className="flex items-center justify-center min-h-screen"></main>
+    <main className="flex flex-col items-center justify-center min-h-screen">
+      <button onClick={() => {
+        api.poke({
+          app: "albums",
+          mark: "albums-action",
+          json: { "create": { "name": "string", "owner": "~zod" } }
+        })
+      }}>
+        make the album
+      </button>
+      <button onClick={() => {
+        api.poke({
+          app: "albums",
+          mark: "albums-action",
+          json: { "add": { "album-id": { "name": "string", "owner": "~zod" }, "img-id": "test", "src": "https://s3.us-east-1.amazonaws.com/haddefsigwen1/2023.5.25..16.56.30-hachikuji_waifu2x_art_scale.png", "caption": { "who": "~nec", "when": "1694190024", "what": "caption" } } }
+        })
+      }}>
+        add the photo
+      </button>
+      <button onClick={() => {
+        api.poke({
+          app: "albums",
+          mark: "albums-action",
+          json: { "nuke": { "name": "string", "owner": "~zod" } }
+
+        })
+      }}>
+        nuke the album
+      </button>
+      <button onClick={() => {
+        api.poke({
+          app: "albums",
+          mark: "albums-action",
+          json: { "comment": { "album-id": { "name": "string", "owner": "~zod" }, "img-id": "test", "comment": { "who": "~nec", "when": "1694190024", "what": "comment" } } }
+        })
+      }}>
+        comment on the photo
+      </button>
+      <button onClick={() => {
+        api.poke({
+          app: "albums",
+          mark: "albums-action",
+          json: { "share": { "album-id": { "name": "string", "owner": "~wex" }, "img-id": "test", "receiver": "~zod" } }
+        })
+      }}>
+        share the photo with someone
+      </button>
+    </main>
   );
 }
