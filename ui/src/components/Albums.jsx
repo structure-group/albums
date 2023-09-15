351e5a14379d1e5b401674618b70de15b43eace6
Matilde Park <matilde@park.net>
2023-09-15 18:39:01 -0400

    albums: conditionally render list of albums by s3 status

---
+++ ui/src/components/Albums.jsx
@@ -0,0 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
+import { albumsQuery } from "../state/query";
+import { Link } from "react-router-dom";
+import useStorageState from "../state/storage";
+export default function Albums() {
+    const { s3 } = useStorageState();
+    const { credentials } = s3;
+    const albums = useQuery({ queryKey: ["albums"], queryFn: albumsQuery });
+    console.log(albums.data)
+    return credentials ?
+        <div className="p-8 flex flex-wrap gap-8">
+            {albums.data?.map(([id, owner]) => {
+                return <Link to={`/album/${owner}/${id}`} key={`${owner}/${id}`}>
+                    <div className="w-64 h-64 bg-gray-200 rounded-xl flex flex-col items-center justify-center">
+                        <p className="text-2xl font-semibold">{id}</p>
+                        <p className="text-sm">{owner}</p>
+                    </div>
+                </Link>
+            })}
+        </div>
+        : <div className="h-full w-full flex flex-col items-center justify-center">
+            <p><a className="font-semibold border-b border-black mr-1" href="https://operators.urbit.org/manual/os/s3" target="_blank">Set up Amazon S3</a>to begin using Albums.</p>
+        </div>
+}
