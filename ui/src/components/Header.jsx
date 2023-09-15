ff3f818e11a84807c7f47363a9328197523e9182
Matilde Park <matilde@park.net>
2023-09-15 18:38:02 -0400

    components: add header and conditional render for add album

---
+++ ui/src/components/Header.jsx
@@ -0,0 +1,24 @@
+import { Link, useLocation } from "react-router-dom";
+import cn from "classnames";
+import useStorageState from "../state/storage";
+
+export default function Header() {
+    const { s3 } = useStorageState();
+    const location = useLocation();
+    const credentials = s3.credentials;
+    return <div className="bg-white border-b border-2 border-indigo-gray flex justify-between px-6 py-4 items-center font-semibold">
+        <p>Albums</p>
+        <div className="flex space-x-12 items-center">
+            <Link to="/" className={cn({
+                "text-[#666666]": location.pathname !== "/",
+            })}>My Albums</Link>
+            <Link to="/shared" className={cn({
+                "text-[#666666]": location.pathname !== "/shared",
+            })}>Shared with Me</Link>
+            <Link to="/new" className={cn("px-4 rounded-lg p-2", {
+                "pointer-events-none bg-[#cccccc] text-[#999999]": !credentials,
+                "text-white bg-[#333333]": credentials
+            })}>New Album</Link>
+        </div>
+    </div>
+}
