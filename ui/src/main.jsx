0cdaeb2d4cc524923d40352e4985bac93f29d6a4
Matilde Park <matilde@park.net>
2023-09-15 18:38:16 -0400

    main: add queryclientprovider

--- ui/src/main.jsx
+++ ui/src/main.jsx
@@ -2,10 +2,15 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./app";
 import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const container = document.getElementById("app");
+const queryClient = new QueryClient();
+
 createRoot(container).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
