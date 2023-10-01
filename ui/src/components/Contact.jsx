32b2c69398b4c567590fc3496895407939fbd20b
Matilde Park <matilde@park.net>
2023-10-01 14:47:21 -0400

    contact: remove writer pass, add mousedown handler

--- ui/src/components/Contact.jsx
+++ ui/src/components/Contact.jsx
@@ -1,21 +1,26 @@
-import { Sigil } from "./Sigil"
-import cn from "classnames"
-export default function Contact({ ship, contact, disabledNicknames, disabledAvatars, writer, className = "" }) {
-    return (
-        <div className={cn("flex space-x-2 items-center w-full", className)}>
-            {!disabledAvatars && contact?.avatar ? (
-                <img src={contact.avatar} className="h-8 w-8 rounded-md" />
-            )
-                : <Sigil
-                    ship={ship}
-                    background="#000000"
-                    foreground="#ffffff"
-                />
-            }
-            <p className="font-semibold max-w-sm truncate min-w-0 text-sm">
-                {!disabledNicknames && contact?.nickname ? contact.nickname : ship}
-            </p>
-            {writer && <p className="text-gray-400 font-semibold">Writer</p>}
-        </div>
-    )
+import { Sigil } from "./Sigil";
+import cn from "classnames";
+export default function Contact({
+  ship,
+  contact,
+  disabledNicknames,
+  disabledAvatars,
+  className = "",
+  onMouseDown = () => { },
+}) {
+  return (
+    <div
+      className={cn("flex space-x-2 items-center w-full", className)}
+      onMouseDown={onMouseDown}
+    >
+      {!disabledAvatars && contact?.avatar ? (
+        <img src={contact.avatar} className="h-8 w-8 rounded-md" />
+      ) : (
+        <Sigil ship={ship} background="#000000" foreground="#ffffff" />
+      )}
+      <p className="font-semibold max-w-sm truncate min-w-0 text-sm">
+        {!disabledNicknames && contact?.nickname ? contact.nickname : ship}
+      </p>
+    </div>
+  );
 }
