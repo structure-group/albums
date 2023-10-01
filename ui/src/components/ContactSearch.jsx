be7098805fabc85244d97269b61d948941bef0c5
Matilde Park <matilde@park.net>
2023-10-01 14:47:34 -0400

    contactsearch: add selectedmembers catches

--- ui/src/components/ContactSearch.jsx
+++ ui/src/components/ContactSearch.jsx
@@ -1,13 +1,25 @@
-import { useState } from "react"
+import { useState } from "react";
 import ob from "urbit-ob";
 import Contact from "./Contact";
 import { deSig } from "@urbit/api";
-export default function ContactSearch({ contacts, group, disabledNicknames, disabledAvatars, setSelectedMembers }) {
+export default function ContactSearch({
+    contacts,
+    group,
+    disabledNicknames,
+    disabledAvatars,
+    selectedMembers,
+    setSelectedMembers,
+}) {
     const [showSearch, setShowSearch] = useState(false);
     const [searchQuery, setSearchQuery] = useState("");
     const groupMembers = group.map((member) => member[0]);
     const isInGroup = groupMembers.some((e) => deSig(e) === deSig(searchQuery));
-    const search = Object.entries(contacts.data || {}).filter(([ship, contact]) => !isInGroup && (ship.includes(searchQuery) || contact?.nickname?.includes(searchQuery)));
+    const search = Object.entries(contacts.data || {}).filter(
+        ([ship, contact]) =>
+            !isInGroup &&
+            searchQuery !== "" &&
+            (ship.includes(searchQuery) || contact?.nickname?.includes(searchQuery)),
+    );
     return (
         <div className="w-full">
             <input
@@ -17,21 +29,71 @@ export default function ContactSearch({ contacts, group, disabledNicknames, disa
                 onFocus={() => setShowSearch(true)}
                 onBlur={() => setShowSearch(false)}
                 onChange={(e) => setSearchQuery(e.target.value)}
+                onKeyDown={(e) => {
+                    if (
+                        e.key === "Enter" &&
+                        !isInGroup &&
+                        ob.isValidPatp(`~${deSig(searchQuery)}`) &&
+                        !selectedMembers.includes(`~${deSig(searchQuery)}`)
+                    ) {
+                        setSelectedMembers((prev) => [...prev, `~${deSig(searchQuery)}`]);
+                        setSearchQuery("");
+                    }
+                }}
+                value={searchQuery}
             />
-            {showSearch && <div className="absolute flex flex-col w-full p-4 border border-indigo-gray max-h-16 overflow-y-auto bg-white rounded-md">
-                {search.map(([ship, contact]) => {
-                    return <Contact
-                        ship={ship}
-                        contact={contact}
-                        disabledNicknames={disabledNicknames}
-                        disabledAvatars={disabledAvatars}
-                        key={ship}
-                        className="hover:bg-indigo-white"
-                    />
-                })}
-                {search.length === 0 && !isInGroup && ob.isValidPatp(`~${deSig(searchQuery)}`) && <Contact ship={`~${deSig(searchQuery)}`} disabledNicknames={disabledNicknames} disabledAvatars={disabledAvatars} className="hover:bg-indigo-gray" />}
-                {(search.length === 0 && !ob.isValidPatp(`~${deSig(searchQuery)}`) || isInGroup) && <p className="text-sm text-gray-400 text-center">No results</p>}
-            </div>}
+            {showSearch && (
+                <div className="absolute flex flex-col w-full p-4 border border-indigo-gray max-h-16 overflow-y-auto bg-white rounded-md">
+                    {search.map(([ship, contact]) => {
+                        return (
+                            <div key={ship} className="w-full z-20 bg-purple-50">
+                                <Contact
+                                    ship={ship}
+                                    contact={contact}
+                                    disabledNicknames={disabledNicknames}
+                                    disabledAvatars={disabledAvatars}
+                                    className="hover:bg-indigo-white"
+                                    onMouseDown={(e) => {
+                                        if (!selectedMembers.includes(ship)) {
+                                            e.preventDefault();
+                                            setSelectedMembers((prev) => [...prev, [ship, false]]);
+                                            setSearchQuery("");
+                                            setShowSearch(false);
+                                        }
+                                    }}
+                                />
+                            </div>
+                        );
+                    })}
+                    {search.length === 0 &&
+                        !isInGroup &&
+                        ob.isValidPatp(`~${deSig(searchQuery)}`) && (
+                            <Contact
+                                ship={`~${deSig(searchQuery)}`}
+                                disabledNicknames={disabledNicknames}
+                                disabledAvatars={disabledAvatars}
+                                className="hover:bg-indigo-gray"
+                                onMouseDown={(e) => {
+                                    if (!selectedMembers.some((e) => e[0] === `~${deSig(searchQuery)}`)) {
+                                        e.preventDefault();
+                                        setSelectedMembers((prev) => [
+                                            ...prev,
+                                            [`~${deSig(searchQuery)}`, false],
+                                        ]);
+                                        setSearchQuery("");
+                                        setShowSearch(false);
+                                    }
+                                }}
+                            />
+                        )}
+                    {((search.length === 0 &&
+                        searchQuery !== "" &&
+                        !ob.isValidPatp(`~${deSig(searchQuery)}`)) ||
+                        isInGroup) && (
+                            <p className="text-sm text-gray-400 text-center">No results</p>
+                        )}
+                </div>
+            )}
         </div>
-    )
+    );
 }
