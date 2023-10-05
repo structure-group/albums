a63c7b76f88d61849e18ed2aef2bf1f91d56c176
Matilde Park <matilde@park.net>
2023-10-05 14:55:08 -0400

    sigil: ignore moons

--- ui/src/components/Sigil.jsx
+++ ui/src/components/Sigil.jsx
@@ -2,6 +2,9 @@ import { sigil, reactRenderer } from "@tlon/sigil-js";
 import { deSig } from "@urbit/api";
 
 export const Sigil = ({ ship, background, foreground }) => {
+  if (ship.length > 14) {
+    return <div />
+  }
   return (
     <div className="p-2 rounded-md" style={{ backgroundColor: background }}>
       {sigil({
