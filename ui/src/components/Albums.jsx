fd1ec0ae7b72addb715433158290aae09de70032
Matilde Park <matilde@park.net>
2023-10-19 15:23:50 -0400

    albums: add archive badge on list view

--- ui/src/components/Albums.jsx
+++ ui/src/components/Albums.jsx
@@ -21,18 +21,20 @@ export default function Albums({ shared = false }) {
           {albums?.data
             ?.filter(shared ? sharedFilter : ourFilter)
             .map((album) => {
-              const { name, title, owner, cover } = album;
+              const { name, title, owner, cover, archive } = album;
               return (
                 <Link to={`/album/${owner}/${name}`} key={`${owner}/${name}`}>
                   <div className="flex flex-col items-center w-64 space-y-2">
                     <div
-                      className="w-[270px] h-[270px] bg-gray-200 rounded-xl flex flex-col items-center justify-center"
+                      className="w-[270px] h-[270px] bg-gray-200 rounded-xl flex flex-col items-center justify-center relative"
                       style={{
                         backgroundImage: cover ? `url(${cover})` : DefaultAlbum,
                         backgroundSize: "cover",
                         backgroundPosition: "center",
                       }}
-                    ></div>
+                    >
+                      {archive && <div className="rounded-[10px] bg-[rgba(30,30,30,0.5)] text-white text-xs font-semibold absolute bottom-4 right-4 px-2 py-1">ARCHIVE</div>}
+                    </div>
                     <p className="font-semibold text-sm w-full text-center">
                       {title}
                     </p>
